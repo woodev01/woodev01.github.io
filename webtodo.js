6b86bbe344391a3bca329e30a88d190a1dada8a5
woodev01 <woodev01@luther.edu>
2015-10-21 13:36:55 -0500

    Update webtodo.js

--- webtodo.js
+++ webtodo.js
@@ -1,1 +1,18 @@
+newbutton = function() {
 
+var ed
+var edd
+var eddie
+
+ed = document.querySelector("#todolist");
+edd = document.querySelector("#task");
+
+t = document.querySelector("#timetable");
+li = document.createElement("li");
+
+contents = document.createTextNode(edd);
+li.appendChild(contents);
+
+ed.appendChild(li);
+
+}
